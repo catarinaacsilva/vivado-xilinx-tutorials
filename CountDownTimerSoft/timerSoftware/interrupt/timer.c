ebf03ad573e4d7faacbf0ef5c744c58e7c2fcdba
Catarina Silva <c.alexandracorreia@ua.pt>
2020-04-15 14:04:47 +0100

    Update timer: interrupt

--- CountDownTimerSoft/timerSoftware/interrupt/timer.c
+++ CountDownTimerSoft/timerSoftware/interrupt/timer.c
@@ -1,49 +1,15 @@
 /******************************************************************************
 *
-* Copyright (C) 2009 - 2014 Xilinx, Inc.  All rights reserved.
+* Timer based on Software - Polling
 *
-* Permission is hereby granted, free of charge, to any person obtaining a copy
-* of this software and associated documentation files (the "Software"), to deal
-* in the Software without restriction, including without limitation the rights
-* to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-* copies of the Software, and to permit persons to whom the Software is
-* furnished to do so, subject to the following conditions:
+* Author: Catarina Silva
+* Email: c.alexandracorreia@ua.pt
+* Email: c.alexandracorreia@av.it.pt
 *
-* The above copyright notice and this permission notice shall be included in
-* all copies or substantial portions of the Software.
-*
-* Use of the Software is limited solely to applications:
-* (a) running on a Xilinx device, or
-* (b) that interact with a Xilinx device through a bus or interconnect.
-*
-* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-* IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-* FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
-* XILINX  BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
-* WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF
-* OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-* SOFTWARE.
-*
-* Except as contained in this notice, the name of the Xilinx shall not be used
-* in advertising or otherwise to promote the sale, use or other dealings in
-* this Software without prior written authorization from Xilinx.
+* Version 1.0
 *
 ******************************************************************************/
 
-/*
- * helloworld.c: simple test application
- *
- * This application configures UART 16550 to baud rate 9600.
- * PS7 UART (Zynq) is not initialized by this application, since
- * bootrom/bsp configures it to baud rate 115200
- *
- * ------------------------------------------------
- * | UART TYPE   BAUD RATE                        |
- * ------------------------------------------------
- *   uartns550   9600
- *   uartlite    Configurable only in HW design
- *   ps7_uart    115200 (configured by bootrom/bsp)
- */
 
 #include <stdio.h>
 #include "xil_printf.h"
@@ -221,19 +187,124 @@ void ReadButtons(TButtonStatus* pButtonStatus)
 void UpdateStateMachine(TFSMState* pFSMState, TButtonStatus* pButtonStatus,
 						bool zeroFlag, unsigned char* pSetFlags)
 {
-	// Insert your code here...
+	switch (*pFSMState) {
+		case Stopped:
+			*pSetFlags = 0x0;
+			if(zeroFlag == 1) {
+				XGpio_WriteReg(XPAR_AXI_GPIO_LEDS_BASEADDR, XGPIO_DATA_OFFSET, 0x0001);
+			}else {
+				XGpio_WriteReg(XPAR_AXI_GPIO_LEDS_BASEADDR, XGPIO_DATA_OFFSET, 0x0000);
+			}
+			if((DetectAndClearRisingEdge(&(pButtonStatus->startPrevious), pButtonStatus->startPressed)) && (zeroFlag == 0)) {
+				*pFSMState = Started;
+			} else if (DetectAndClearRisingEdge(&(pButtonStatus->setPrevious), pButtonStatus->setPressed)) {
+				*pFSMState = SetLSSec;
+			} else {
+				*pFSMState = Stopped;
+			}
+		break;
+
+		case Started:
+			*pSetFlags = 0x0;
+			if ((DetectAndClearRisingEdge(&(pButtonStatus->startPrevious), pButtonStatus->startPressed)) || (zeroFlag == 1)) {
+				*pFSMState = Stopped;
+			} else {
+				*pFSMState = Started;
+			}
+		break;
+
+		case SetLSSec:
+			*pSetFlags = 0x1;
+			if(DetectAndClearRisingEdge(&(pButtonStatus->setPrevious), pButtonStatus->setPressed)) {
+				*pFSMState = SetMSSec;
+			} else {
+				*pFSMState = SetLSSec;
+			}
+		break;
+
+		case SetMSSec:
+			*pSetFlags = 0x2;
+			if(DetectAndClearRisingEdge(&(pButtonStatus->setPrevious), pButtonStatus->setPressed)) {
+				*pFSMState = SetLSMin;
+			} else {
+				*pFSMState = SetMSSec;
+			}
+		break;
+
+		case SetLSMin:
+			*pSetFlags = 0x4;
+			if(DetectAndClearRisingEdge(&(pButtonStatus->setPrevious), pButtonStatus->setPressed)) {
+				*pFSMState = SetMSMin;
+			} else {
+				*pFSMState = SetLSMin;
+			}
+		break;
+
+		case SetMSMin:
+			*pSetFlags = 0x8;
+			if(DetectAndClearRisingEdge(&(pButtonStatus->setPrevious), pButtonStatus->setPressed)) {
+				*pFSMState = Stopped;
+			} else {
+				*pFSMState = SetMSMin;
+			}
+		break;
+		default:
+			*pSetFlags = 0x0; 	
+			*pFSMState = Stopped;
+			break;
+	}
 
 }
 
 void SetCountDownTimer(TFSMState fsmState, const TButtonStatus* pButtonStatus, TTimerValue* pTimerValue)
 {
-	// Insert your code here...
-
+	switch(fsmState){
+		case SetLSSec:
+			if(pButtonStatus->upPressed)
+				ModularInc(&(pTimerValue->secLSValue), 10);
+			else if (pButtonStatus->downPressed)
+				ModularDec(&(pTimerValue->secLSValue), 10);
+			break;
+		case SetMSSec:
+			if(pButtonStatus->upPressed)
+				ModularInc(&(pTimerValue->secMSValue), 6);
+			else if (pButtonStatus->downPressed)
+				ModularDec(&(pTimerValue->secMSValue), 6);
+			break;
+		case SetLSMin:
+			if(pButtonStatus->upPressed)
+				ModularInc(&(pTimerValue->minLSValue), 10);
+			else if (pButtonStatus->downPressed)
+				ModularDec(&(pTimerValue->minLSValue), 10);
+			break;
+		case SetMSMin:
+			if(pButtonStatus->upPressed)
+				ModularInc(&(pTimerValue->minMSValue), 6);
+			else if (pButtonStatus->downPressed)
+				ModularDec(&(pTimerValue->minMSValue), 6);
+			break;
+		default:
+			break;
+	}
 }
 
 void DecCountDownTimer(TFSMState fsmState, TTimerValue* pTimerValue)
 {
-	// Insert your code here...
+	if (fsmState == Started) {													
+		bool count = ModularDec(&pTimerValue->secLSValue, 10);		
+		if (count) {
+			count = ModularDec(&pTimerValue->secMSValue, 6);		
+			if (count) {
+				count = ModularDec(&pTimerValue->minLSValue, 10);	
+				if (count) {
+					ModularDec(&pTimerValue->minMSValue, 6); 
+				}
+				else return;
+			}
+			else return;
+		}
+		else return;
+	}
 
 }
 
