0e0baa1fb00757c4e7cb5b4b4bb457a4e958fc66
Catarina Silva <catarinaacorreiasilva@gmail.com>
2020-06-21 21:01:36 +0100

    Finish guide 5 with interrupts

--- timer-hard-soft/interrupt/CustomTimerDemo/CustomTimerDemo.runs/impl_1/runme.sh
+++ timer-hard-soft/interrupt/CustomTimerDemo/CustomTimerDemo.runs/impl_1/runme.sh
@@ -24,7 +24,7 @@ else
 fi
 export LD_LIBRARY_PATH
 
-HD_PWD='C:/Users/catar/Desktop/git/vivado-xilinx-tutorials/timer-hard-soft/CustomTimerDemo/CustomTimerDemo.runs/impl_1'
+HD_PWD='C:/Users/catar/Desktop/git/vivado-xilinx-tutorials/timer-hard-soft/interrupt/CustomTimerDemo/CustomTimerDemo.runs/impl_1'
 cd "$HD_PWD"
 
 HD_LOG=runme.log
@@ -41,7 +41,7 @@ EAStep()
 }
 
 # pre-commands:
-/bin/touch .init_design.begin.rst
+/bin/touch .write_bitstream.begin.rst
 EAStep vivado -log mb_design_wrapper.vdi -applog -m64 -product Vivado -messageDb vivado.pb -mode batch -source mb_design_wrapper.tcl -notrace
 
 
